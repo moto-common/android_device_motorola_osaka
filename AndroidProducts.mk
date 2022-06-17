827cb336a3b474a79d096883551fa740e7275bbf
electimon <electimon@gmail.com>
2022-06-17 05:26:09 +0000

    osaka: Introduce hentai_osaka product
    Signed-off-by: electimon <electimon@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_osaka.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_osaka.mk $(LOCAL_DIR)/hentai_osaka.mk
 
 COMMON_LUNCH_CHOICES += \
     aosp_osaka-eng \
