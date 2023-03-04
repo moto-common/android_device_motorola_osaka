6eb6c273b382a7039cde4584c72cbdcdf3537df6
electimon <electimon@gmail.com>
2023-03-04 13:03:49 +0800

    osaka: Modernize RROs
    Signed-off-by: electimon <electimon@gmail.com>

--- device.mk
+++ device.mk
@@ -15,11 +15,6 @@
 # Device path
 DEVICE_PATH := device/motorola/osaka/rootdir
 
-# Overlay
-PRODUCT_PACKAGES += \
-    osakaFrameworkOverlay \
-    osakaSystemUIOverlay
-
 # Device Specific Permissions
 PRODUCT_COPY_FILES := \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
